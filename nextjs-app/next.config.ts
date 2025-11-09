33f34fb2afef5b930e75f4b7026023a56b351d24
nclaira <nclaire981@gmail.com>
2025-11-09 15:47:57 +0200

    update Next.js config

--- nextjs-app/next.config.ts
+++ nextjs-app/next.config.ts
@@ -1,8 +1,23 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+
+
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    domains: ['images.unsplash.com'],
+    unoptimized: true, // Required when using 'output: export'
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
